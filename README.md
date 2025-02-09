c835edc3cc296af93b459c4bf22b6d70c8873fca
JosephCaillet <10532348+JosephCaillet@users.noreply.github.com>
2025-02-09 20:45:34 +0100

    Basic page with ability to display IPv4 in multiple bases, byte by byte.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# ip-address-playground
+# IP address playground
+
 A web application playground that aims at experimenting with IP addresses.
+
+Web app (work in progress) live [here](https://josephcaillet.github.io/ip-address-playground/)!
+
+All rights reserved for now.
+
+Copyright (c) 2025 Joseph Caillet
---
+++ script.js
@@ -0,0 +1,161 @@
+"use strict";
+
+class IpAddress {
+	constructor(ip, base = 10) {
+
+		// Check IP version
+		if (ip.includes(".")) {
+			// IPv4
+			this.version = 4
+		} else if (ip.includes(":")) {
+			// IPv6
+			this.version = 6
+		} else {
+			throw new Error("Invalid IP: nor IPv4 nor IPv6")
+		}
+
+		// Check each bytes group
+		let bytesGroupsNumber = this.isIpv4() ? 4 : 8
+		this.bytesGroups = ip.split(this.getBytesGroupsSeparator())
+
+		// Handle "::" IPv6 notation, and reject it's IPv4 equivalent
+		if (this.isIpv4() && ip.includes("..")) {
+			throw new Error("Invalid IPv4: \".\" is seen more that once in row")
+		}
+		if (this.isIpv6() && ip.includes("::")) {
+			if (ip.includes(":::")) {
+				throw new Error("Invalid IPv6: \":\" is seen more that twice in row")
+			}
+			ip = ip.replace("::", ":".repeat(2 + bytesGroupsNumber - this.bytesGroups.length))
+			this.bytesGroups = ip.split(this.getBytesGroupsSeparator())
+		}
+
+		// Check bytes group number
+		if (this.bytesGroups.length != bytesGroupsNumber) {
+			throw new Error("Invalid IP: bad bytes groups number")
+		}
+
+		// Check each bytes group is a number within allowed range
+		for (let i = 0; i < this.bytesGroups.length; i++) {
+			// Handle IPv6 "::" notation
+			if (this.isIpv6() && this.bytesGroups[i] === "") {
+				this.bytesGroups[i] = 0
+			}
+
+			// Check is valid number
+			let byteGroupValue = parseInt(this.bytesGroups[i], base)
+			if (Number.isNaN(byteGroupValue)) {
+				throw new Error(`Invalid IP: bytes group number ${i + 1} is not a base ${base} number`)
+			}
+
+			// Check is within range
+			let max = this.isIpv4() ? 2 ** 8 : 2 ** 16
+			if (byteGroupValue < 0 || byteGroupValue > max) {
+				throw new Error(`Invalid IP: bytes group number ${i + 1} must be a number between 0 and ${max}`)
+			}
+		}
+	}
+
+	isIpv4() {
+		return this.version == 4
+	}
+
+	isIpv6() {
+		return this.version == 6
+	}
+
+	getBytesGroupsSeparator() {
+		return this.isIpv4() ? "." : ":"
+	}
+
+	// TODO: add support for IPv6 compressed notation
+	getBytesGroupsString(base = 10) {
+		return this.bytesGroups.map(bg => Number(bg).toString(base))
+	}
+
+	getIpString(base = 10) {
+		return this.getBytesGroupsString(base).join(this.getBytesGroupsSeparator())
+	}
+}
+
+class IpPanel extends HTMLElement {
+	constructor() {
+		super()
+		this.base = 10
+	}
+
+	connectedCallback() {
+		this.base = this.getAttribute("base")
+
+		// TODO: fix selection (one should be able to select the first displayed line only)
+		this.innerHTML = `
+			<div class="div-cntnr">
+				<span>
+					<div class="display">0</div>
+					<div><input type="text"></div>
+				</span>
+				<span>
+					<div>.</div>
+					<div>.</div>
+				</span>
+				<span>
+					<div class="display">0</div>
+					<div><input type="text"></div>
+				</span>
+				<span>
+					<div>.</div>
+					<div>.</div>
+				</span>
+				<span>
+					<div class="display">0</div>
+					<div><input type="text"></div>
+				</span>
+				<span>
+					<div>.</div>
+					<div>.</div>
+				</span>
+				<span>
+					<div class="display">0</div>
+					<div><input type="text"></div>
+				</span>
+			</div>`
+	}
+
+	setIp(ip) {
+		// TODO: find a way to display base 2 with leading zeros
+		let bytesGroups = ip.getBytesGroupsString(this.base)
+		let displays = Array.from(this.querySelectorAll(".display"))
+		let inputs   = Array.from(this.querySelectorAll("input"))
+
+		for (let i = 0; i < bytesGroups.length; i++) {
+			displays[i].innerText = bytesGroups[i]
+			inputs[i].value = bytesGroups[i]
+		}
+	}
+}
+
+customElements.define("ip-panel", IpPanel)
+
+document.addEventListener("DOMContentLoaded", main)
+
+function main() {
+	document.querySelector("#mainIpInput").addEventListener("input", (e) => {
+		refreshIp(e.target.value)
+	})
+
+	// First refresh init
+	refreshIp(document.querySelector("#mainIpInput").value )
+}
+
+function refreshIp(ip) {
+	let ipAddr
+	try {
+		ipAddr = new IpAddress(ip)
+		document.querySelector("#errorMessage").innerText = ""
+	} catch (err) {
+		return document.querySelector("#errorMessage").innerText = err.message
+	}
+
+	document.querySelector("#mainIpDisplay").innerText = ipAddr.getIpString()
+	document.querySelectorAll("ip-panel").forEach(ipp => ipp.setIp(ipAddr))
+}
